82eb4aa7c762745eebaba12da73d718b310daa9a
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2020-10-26 15:48:57 +0100

    Add project files.

---
+++ MintPlayer.Xamarin.Forms.Controls/CheckBox.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MintPlayer.Xamarin.Forms.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CheckBox : ContentView
+    {
+        public CheckBox()
+        {
+            InitializeComponent();
+        }
+
+        #region IsChecked
+        public static readonly BindableProperty IsCheckedProperty = BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(CheckBox), propertyChanged: (bindable, oldValue, newValue) =>
+        {
+
+        });
+        public bool IsChecked
+        {
+            get => (bool)GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+        #endregion
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            IsChecked = !IsChecked;
+        }
+    }
+}
