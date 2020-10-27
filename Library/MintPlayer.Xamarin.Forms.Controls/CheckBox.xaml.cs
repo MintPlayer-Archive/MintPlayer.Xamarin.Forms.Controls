a9194367609d1d2cce431d5734791f076da9fc68
DESKTOP-K4FBQRK\piete <pieterjandeclippel@msn.com>
2020-10-27 09:43:03 +0100

    Working bindings

--- Library/MintPlayer.Xamarin.Forms.Controls/CheckBox.xaml.cs
+++ Library/MintPlayer.Xamarin.Forms.Controls/CheckBox.xaml.cs
@@ -11,7 +11,7 @@ namespace MintPlayer.Xamarin.Forms.Controls
         public CheckBox()
         {
             InitializeComponent();
-            //BindingContext = this;
+            BindingContext = this;
 
             //var counter = 0;
             //Task.Run(async () =>
@@ -25,30 +25,40 @@ namespace MintPlayer.Xamarin.Forms.Controls
             //});
         }
 
-        //#region IsChecked
-        //public static readonly BindableProperty IsCheckedProperty = BindableProperty.Create("IsChecked", typeof(bool), typeof(CheckBox), propertyChanged: (bindable, oldValue, newValue) =>
-        //{
-
-        //});
+        #region IsChecked
+        public static readonly BindableProperty IsCheckedProperty = BindableProperty.Create("IsChecked", typeof(bool), typeof(CheckBox), false, BindingMode.TwoWay, propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            //global::Xamarin.Forms.CheckBox c;c.CheckedChanged
+        });
 
         //public bool IsChecked
         //{
         //    get => (bool)GetValue(IsCheckedProperty);
-        //    set => SetValue(IsCheckedProperty, value);
+        //    set
+        //    {
+        //        SetValue(IsCheckedProperty, value);
+        //        OnPropertyChanged();
+        //    }
         //}
-        //#endregion
+
+        private bool tada;
+        public bool Tada
+        {
+            get { return tada; }
+            set
+            {
+                tada = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
 
         #region Text
-        public static readonly BindableProperty TextProperty = BindableProperty.Create(
-            "Text",
-            typeof(string),
-            typeof(CheckBox),
-            "",
-            BindingMode.OneWay,
-            propertyChanged: (bindable, oldValue, newValue) =>
-            {
+        public static readonly BindableProperty TextProperty = BindableProperty.Create("Text", typeof(string), typeof(CheckBox), "", BindingMode.OneWay, propertyChanged: (bindable, oldValue, newValue) =>
+        {
                 
-            });
+        });
+
         public string Text
         {
             get => (string)GetValue(TextProperty);
@@ -56,9 +66,9 @@ namespace MintPlayer.Xamarin.Forms.Controls
         }
         #endregion
 
-        //private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
-        //{
-        //    IsChecked = !IsChecked;
-        //}
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            //IsChecked = !IsChecked;
+        }
     }
 }
